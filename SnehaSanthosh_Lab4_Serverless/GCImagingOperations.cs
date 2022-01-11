3526564ed6b8d5aee290ce0d8dd211bafd0c92fb
Sneha Santhosh <45783979+Sneha-Santhosh@users.noreply.github.com>
2022-01-11 00:23:26 -0500

    Add all files

---
+++ SnehaSanthosh_Lab4_Serverless/GCImagingOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using GrapeCity.Documents.Drawing;
+using GrapeCity.Documents.Text;
+using GrapeCity.Documents.Imaging;
+
+namespace SnehaSanthosh_Lab4_Serverless
+{
+    class GCImagingOperations
+    {
+        public static string GetConvertedImage(byte[] stream)
+        {
+            using (var bmp = new GcBitmap())
+            {
+                bmp.Load(stream);
+                // Add watermark 
+                var newImg = new GcBitmap();
+                newImg.Load(stream);
+                using (var g = bmp.CreateGraphics(Color.White))
+                {
+                    g.DrawImage(
+                       newImg,
+                       new RectangleF(0, 0, bmp.Width, bmp.Height),
+                       null,
+                       ImageAlign.Default
+                       );
+                }
+                //  Convert to grayscale 
+                bmp.ApplyEffect(GrayscaleEffect.Get(GrayscaleStandard.BT601));
+                //  Resize to thumbnail 
+                var resizedImage = bmp.Resize(100, 100, InterpolationMode.NearestNeighbor);
+                return GetBase64(resizedImage);
+            }
+        }
+        #region helper 
+        private static string GetBase64(GcBitmap bmp)
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                bmp.SaveAsPng(m);
+                return Convert.ToBase64String(m.ToArray());
+            }
+        }
+        #endregion
+    }
+}
---
+++ SnehaSanthosh_Lab4_Serverless/ImagesDB.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnehaSanthosh_Lab4_Serverless
+{
+
+    public class ImagesDB
+    {
+        public int ImageID { get; set; }
+        public string ImageKey { get; set; }
+        public Dictionary<string, AttributeValue> Labels { get; set; }
+    
+    }
+
+}
+
